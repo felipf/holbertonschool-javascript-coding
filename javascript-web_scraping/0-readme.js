424e87176d781f4979405464ee5f1e37b72a535b
felipf <felipereiraforte@gmail.com>
2023-09-24 17:15:14 -0700

    tasks

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const fs = require("fs");
+const fs = require('fs');
 
 const filePath = process.argv[2];
 
-fs.readFile(filePath, "utf-8", (err, data) => {
+fs.readFile(filePath, 'utf-8', (err, data) => {
   if (err) {
     return console.error(err);
   }
