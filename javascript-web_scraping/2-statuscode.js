544fba0b035182a9ebf58bf0f3f235a23e40c7c4
felipf <felipereiraforte@gmail.com>
2023-09-24 17:21:22 -0700

    task done and fixed

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -4,9 +4,9 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, function (error, response) {
-    if (error) {
-        console.error('error:', error);
-    } else {
-        console.log('code:', response.statusCode);
-    }
+  if (error) {
+    console.error('error:', error);
+  } else {
+    console.log('code:', response.statusCode);
+  }
 });
